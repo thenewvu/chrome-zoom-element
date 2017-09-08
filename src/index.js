30c1bb87a5e967c9c135f93e66447943605c9115
Vu Le <thenewvu@gmail.com>
2017-09-09 00:02:49 +0700

    Highligh targeted element when holding

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
+/* global zoom */
+
 (() => {
-  const DURATION = 1000
-
-  let zoomed = false
-  function toggleZoom (element) {
-    zoomed
-      ? zoom.out()
-      : zoom.to({ element })
-    zoomed = !zoomed
-  }
-
+  const DURATION = 700
   let heldAt = 0
 
   document.body.addEventListener('mousedown', ev => {
     heldAt = Date.now()
+    ev.target.classList.add('zoom-target')
   })
 
   document.body.addEventListener('mouseup', ev => {
     if (window.getSelection().toString()) return
 
+    ev.target.classList.remove('zoom-target')
     const heldDuration = Date.now() - heldAt
     if (heldDuration > DURATION) {
       ev.preventDefault()
-      toggleZoom(ev.target)
+      zoom.to({ element: ev.target })
     }
   })
 })()
