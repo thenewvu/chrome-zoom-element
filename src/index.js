bd11d87d5a7cd5a136b8f6f8a8bfe017809d9796
Vu Le <thenewvu@gmail.com>
2017-09-09 08:56:59 +0700

    Don't zoom if the mouse moved during holding a distance that bigger a threshold (5)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@
 
 (() => {
   const MIN_DURATION = 700
-  let downAt = 0
+  let lastNow = null
+  let lastPos = null
   let element = null
+
+  const distance = (p1, p2) => Math.sqrt(
+    Math.pow(Math.abs(p1.x - p2.x), 2) +
+    Math.pow(Math.abs(p1.y - p2.y), 2)
+  )
 
   document.body.addEventListener('mousedown', ev => {
     if (ev.button !== 0) return
-    downAt = Date.now()
+    lastNow = Date.now()
+    lastPos = { x: ev.screenX, y: ev.screenY }
     ev.target.classList.add('zoom-target')
     element = ev.target
   })
@@ -16,8 +23,9 @@
     if (ev.button !== 0) return
     element.classList.remove('zoom-target')
 
+    if (distance(lastPos, {x: ev.screenX, y: ev.screenY}) > 5) return
     if (window.getSelection().toString()) return
-    const duration = Date.now() - downAt
+    const duration = Date.now() - lastNow
     if (duration > MIN_DURATION) {
       ev.preventDefault()
       zoom.to({ element })
